588ab912d2a8b96e96ac9c9b67e3847d7e89ddb7
joshuaalbert <albert@strw.leidenuniv.nl>
2017-10-16 13:34:15 +0200

    sped up calc_rays

--- src/ionotomo/geometry/calc_rays.py
+++ src/ionotomo/geometry/calc_rays.py
@@ -62,7 +62,7 @@ def cast_ray(batch, fermat, tmax, N):
     '''Calculates ray trajectories.
     batch : list
         [origins, directions] where origins and directions are of shape 
-        (Na, Nt, 3) or (Nt, Nd, 3) in the frame of model.
+        (Na, Nt, Nd, 3) in the frame of model.
     fermat : Fermat class object
         Contains the machinery to integrate rays in 3d space.
     tmax : float
@@ -72,23 +72,25 @@ def cast_ray(batch, fermat, tmax, N):
         observation to calculate ray points.
     '''
     origins, directions = batch
+    Na, Nt, Nd, _ = origins.shape
     #origins.shape = [Na,Nt,3] or [Nt,Nd,3]
     #add x,y,z,s
-    shape = list(origins.shape[:-1]) + [4,N]
-    rays = np.zeros(shape,dtype=np.double)
-    #fermat = Fermat(ne_tci=ne_tci,frequency = frequency,type='z',straight_line_approx=straight_line_approx)
-    #log.info("Casting {} rays".format(shape[0]*shape[1]))
+    shape = (Na, Nt, Nd, 4, N)
+    rays = np.zeros(shape, dtype=float)
     i = 0
     while i < shape[0]:
         j = 0
         while j < shape[1]:
-            origin = origins[i,j,:]
-            direction = directions[i,j,:]
-            x,y,z,s = fermat.integrate_ray(origin,direction,tmax,N=N)
-            rays[i,j,0,:] = x
-            rays[i,j,1,:] = y
-            rays[i,j,2,:] = z
-            rays[i,j,3,:] = s
+            k = 0
+            while k < shape[2]:
+                origin = origins[i,j,k,:]
+                direction = directions[i,j,k,:]
+                x,y,z,s = fermat.integrate_ray(origin,direction,tmax,N=N)
+                rays[i,j,k,0,:] = x
+                rays[i,j,k,1,:] = y
+                rays[i,j,k,2,:] = z
+                rays[i,j,k,3,:] = s
+                k += 1
             j += 1
         i += 1
     return rays
@@ -102,36 +104,7 @@ def merge_rays(*ray_bundles):
 
 def calc_rays_dask(antennas,patches,times, array_center, fixtime, phase, ne_tci, frequency,  straight_line_approx,tmax, N= None, get=None):
     '''Do rays in parallel processes batch by directions'''
-    if isinstance(ne_tci,Solution):
-        ne_tci = ne_tci.tci
-    if get is None:
-        get = Client().get
-    if N is None:
-        N = ne_tci.nz
-    Na = len(antennas)
-    Nt = len(times)
-    Nd = len(patches)
-    log.info("Casting rays: {}".format(Na*Nt*Nd))
-    #rays = np.zeros([Na, Nt, Nd, 4, N], dtype= np.double)
-    #split over smaller to make largest workloads
-    if Na < Nd:
-        log.info("spliting over antennas")
-        batches = [delayed(split_antennas)(times, patches, antenna, array_center, fixtime, phase) for antenna in antennas]
-    else:
-        log.info("splitting over directions")
-        batches = [delayed(split_patches)(antennas,times, patch_dir, array_center, fixtime, phase) for patch_dir in patches]
-    fermat = Fermat(ne_tci=ne_tci,frequency = frequency,type='z',straight_line_approx=straight_line_approx)
-    if Na < Nd:
-        #[Nt,Nd,4,N]
-        rays = da.stack([da.from_delayed(delayed(cast_ray)(batch, fermat, tmax, N),
-                                         (Nt,Nd,4,N),dtype=np.double) for batch in batches],axis=0)
-    else:
-        #[Na,Nt,4,N]
-        rays = da.stack([da.from_delayed(delayed(cast_ray)(batch, fermat, tmax, N),
-                                         (Na,Nt,4,N),dtype=np.double) for batch in batches],axis=2)
-    rays = rays.compute(get=get)
-    #log.info(rays)
-    return rays
+    pass 
 
 def calc_rays(antennas,patches,times, array_center, fixtime, phase, ne_tci, frequency,  straight_line_approx,tmax, N=None):
     '''Do rays in parallel processes batch by directions'''
@@ -145,19 +118,29 @@ def calc_rays(antennas,patches,times, array_center, fixtime, phase, ne_tci, freq
 
     #rays = np.zeros([Na, Nt, Nd, 4, N], dtype= np.double)
     #split over smaller to make largest workloads
-    if Na < Nd:
-        log.info("spliting over antennas")
-        batches = [split_antennas(times, patches, antenna, array_center, fixtime, phase) for antenna in antennas]
-    else:
-        log.info("splitting over directions")
-        batches = [split_patches(antennas,times, patch_dir, array_center, fixtime, phase) for patch_dir in patches]
+
+    origins = np.zeros([Na, Nt, Nd,3],dtype=np.double)
+    directions = np.zeros([Na, Nt, Nd,3],dtype=np.double)
+    j = 0
+    while j < Nt:
+        time = times[j]
+        pointing = Pointing(location = array_center.earth_location,
+                            obstime = time, fixtime = fixtime, phase = phase)
+        # Nd x 3
+        dirs = patches.transform_to(pointing).cartesian.xyz.value.transpose()
+        # Na x 3
+        origin = antennas.transform_to(pointing).cartesian.xyz.to(au.km).value.transpose()
+        # Na x Nd x 3
+        # Na x 1  x 3
+        origins[:,j,:, :] += np.expand_dims(origin,1)
+        # Na x Nd x 3
+        #      Nd x 3
+        directions[:,j, :, :] += dirs
+        j += 1
+    batch = (origins,directions)
+    
     fermat = Fermat(ne_tci=ne_tci,frequency = frequency,type='z',straight_line_approx=straight_line_approx)
-    if Na < Nd:
-        #[Nt,Nd,4,N]
-        rays = np.stack([cast_ray(batch, fermat, tmax, N) for batch in batches],axis=0)
-    else:
-        #[Na,Nt,4,N]
-        rays = np.stack([cast_ray(batch, fermat, tmax, N) for batch in batches],axis=2)
-    #log.info(rays)
+    rays = cast_ray(batch, fermat, tmax, N)
+    
     return rays
 
