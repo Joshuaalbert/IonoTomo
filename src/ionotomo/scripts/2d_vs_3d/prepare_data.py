1cacebd348f647b318f9785a72bb62fad658824b
joshuaalbert <albert@strw.leidenuniv.nl>
2017-10-31 18:26:39 +0100

    xstar coords

--- src/ionotomo/scripts/2d_vs_3d/prepare_data.py
+++ src/ionotomo/scripts/2d_vs_3d/prepare_data.py
@@ -28,6 +28,7 @@ if __name__=="__main__":
 #    plot("datapack_vanWeeren_partial_v1.hdf5")
 #    exit()
     datapack = DataPack(filename="../rvw_data_analysis/rvw_datapack.hdf5")
+    
     antennas,antenna_labels = datapack.get_antennas(ant_idx=-1)
     times,timestamps = datapack.get_times(time_idx=-1)
     directions,patch_names = datapack.get_directions(dir_idx=-1)
@@ -81,4 +82,52 @@ if __name__=="__main__":
             _replicate((Na,1),(Nf,),dirs_uvw.v.value),
             _replicate((Na,1),(Nf,),dirs_uvw.w.value), 
             _replicate((Na,1,Nd),(),freqs)],axis=-1)
+
+    datapack_screen = phase_screen_datapack(10,datapack=datapack)
+
+    antennas,antenna_labels = datapack_screen.get_antennas(ant_idx=-1)
+    times,timestamps = datapack_screen.get_times(time_idx=-1)
+    directions,patch_names = datapack_screen.get_directions(dir_idx=-1)
+    freqs = datapack_screen.get_freqs(freq_idx=-1)
+    phase = datapack_screen.get_phase(ant_idx=-1,time_idx=-1,dir_idx=-1,freq_idx=-1)
+
+    Na = len(antennas)
+    Nt = len(times)
+    Nd = len(directions)
+    Nf = len(freqs)
+    print(Na*Nt*Nd*Nf*8*8/1024/1024)
+
+    output['/data/rays_uvw_star'] = np.zeros((Na,Nt,Nd,Nf,8),dtype=float)
+    output['/data/phase_ystar'] = np.zeros_like(phase)
+    output['/data/phase_var'] = np.zeros_like(phase)
+    
+    dt = h5py.special_dtype(vlen=str)
+    antenna_labels_ = output.create_dataset("/data/antenna_labels_star",(Na,),dtype=dt)
+    timestamps_ = output.create_dataset("/data/timestamps_star",(Nt,),dtype=dt)
+    patch_names_ = output.create_dataset("/data/patch_names_star",(Nd,),dtype=dt)
+    antenna_labels_[:] = antenna_labels
+    timestamps_[:] = timestamps
+    patch_names_[:] = patch_names
+
+    for i,t in enumerate(times):
+        print("Working on {}".format(timestamps[i]))
+        phase_center = datapack_screen.get_center_direction()
+        fixtime = times[0]
+        obstime = t
+        center = datapack_screen.radio_array.get_center()
+        uvw = Pointing(location=center, phase= phase_center, fixtime=fixtime, obstime=t)
+        ants_uvw = antennas.transform_to(uvw)
+        dirs_uvw = directions.transform_to(uvw)
+
+        #Na,1,Nd,Nf,8
+        output['/data/rays_uvw_star'][:,i:i+1,:,:,:] = np.stack([
+            _replicate((),(1, Nd, Nf),ants_uvw.u.to(au.km).value),
+            _replicate((),(1, Nd, Nf),ants_uvw.v.to(au.km).value),
+            _replicate((),(1, Nd, Nf),ants_uvw.w.to(au.km).value),
+            _replicate((Na,),(Nd,Nf),times[i:i+1].gps),
+            _replicate((Na,1),(Nf,),dirs_uvw.u.value),
+            _replicate((Na,1),(Nf,),dirs_uvw.v.value),
+            _replicate((Na,1),(Nf,),dirs_uvw.w.value), 
+            _replicate((Na,1,Nd),(),freqs)],axis=-1)
+
     output.close()
