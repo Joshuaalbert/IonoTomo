da19b953da436da6ba48f0b68667253ac0ed6d30
joshuaalbert <albert@strw.leidenuniv.nl>
2017-11-05 15:26:56 +0100

    slow gain variance

--- src/ionotomo/scripts/rvw_data_analysis/load_rvw_data.py
+++ src/ionotomo/scripts/rvw_data_analysis/load_rvw_data.py
@@ -24,7 +24,7 @@ else:
 tec_conversion = -8.4480e9# rad Hz/tecu
 
 
-def import_data(dd_file, di_file, datapack_file, clobber=False):
+def import_data(dd_file, di_file, slow_gain, datapack_file, clobber=False):
     """Create a datapack from the direction (de)independent files.
     dd_file : str
         path to dd solutions made by NDPPP
@@ -37,6 +37,7 @@ def import_data(dd_file, di_file, datapack_file, clobber=False):
     """
     f_dd = h5py.File(dd_file,"r",libver="earliest")
     f_di = h5py.File(di_file,"r")
+    f_sg = h5py.File(slow_gain,"r")
 
     assert (os.path.isfile(datapack_file) and clobber) or (not os.path.isfile(datapack_file)), "datapack file {} already exists and clobber is False".format(datapack_file)
 
@@ -73,7 +74,8 @@ def import_data(dd_file, di_file, datapack_file, clobber=False):
     timestamps = times.isot
     Nt = len(times)
 
-    freqs = f_di['/sol000/phasesoffset000/freq'][...]#Hz
+    #freqs = f_di['/sol000/phasesoffset000/freq'][...]#Hz
+    freqs = f_sg['/sol000/phase000/freq'][...]
     Nf = len(freqs)
 
     data_dict = {"radio_array":radio_array,"times":times, "timestamps": timestamps, "directions": directions, "patch_names": patch_names, "antennas": radio_array.get_antenna_locs(), "antenna_labels": radio_array.get_antenna_labels(), 'freqs': freqs}
@@ -87,8 +89,9 @@ def import_data(dd_file, di_file, datapack_file, clobber=False):
 #            for k in range(Nd):
                 #di part
 
-    phaseoffset_di = np.einsum('ilj,k->ijkl',
-            f_di['/sol000/phasesoffset000/val'][0,0,:,:,:],
+    phaseoffset_di = np.einsum('ij,l,k->ijkl',
+            f_di['/sol000/phasesoffset000/val'][0,0,:,0,:],
+            np.ones(Nf,dtype=float),
             np.ones(Nd,dtype=float))
 
     tec_di = tec_conversion*np.einsum('ji,k,l->ijkl',
@@ -109,25 +112,37 @@ def import_data(dd_file, di_file, datapack_file, clobber=False):
             f_dd['/sol000/tec000/val'][:,:,:,0], 
             freqs_inv)
 
+    #uncertainty estimates
+    slow_gain_phases = f_sg['/sol000/phase000/val'][:,:,:,:,0]
+    var_phase = np.var(slow_gain_phases,axis=1)
+    uncert = np.interp(var_phase,
+            np.percentile(var_phase,np.linspace(0,1,100)),
+            np.linspace(0,1,100)**2 * (np.pi - 0.03) + 0.03)
+    
+
+    #ijkl 
     phase = phaseoffset_di + tec_di + clock_di + scalarphase_dd + tec_dd
 
     #fill out some of the values
-    prop = tec_dd# + scalarphase_dd#tec_di + tec_dd#radians
+    variance = uncert**2
+    prop = tec_dd#+ scalarphase_dd#tec_di + tec_dd#radians
     clock = f_di['/sol000/clock000/val'][:,:].T#seconds
     const = np.mean(np.mean(np.mean((phaseoffset_di + scalarphase_dd)[:,:,:,:],axis=3),axis=2),axis=1)#radians
-    data_dict.update({'phase':phase, 'prop':prop, 'clock':clock, 'const':const})
+    data_dict.update({'phase':phase, 'prop':prop, 'clock':clock, 'const':const, 'variance':variance})
     datapack = DataPack(data_dict)
     datapack.set_reference_antenna(antenna_labels[0])
 
     datapack.save(datapack_file)
     f_dd.close()
     f_di.close()
+    f_sg.close()
     return datapack
 
 if __name__=='__main__':
     import_data("../../data/NsolutionsDDE_2.5Jy_tecandphasePF_correctedlosoto.hdf5",
             "../../data/DI.circ.hdf5",
-            "rvw_datapack_only_tec_dd.hdf5",
+            "../../data/slow_gains.hdf5",
+            "rvw_datapack_full_phase.hdf5",
             clobber=True)
 #    from ionotomo.plotting.plot_tools import plot_datapack, animate_datapack
 #    datapack = DataPack(filename="rvw_datapack.hdf5")
